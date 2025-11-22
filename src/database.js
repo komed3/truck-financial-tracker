b0fca3ddb12036a6b8e6a83f5d7b85ac46eb4ba8
komed3 <pk97@outlook.de>
2025-11-22 10:30:48 +0100

    Create database.js

---
+++ src/database.js
@@ -0,0 +1,43 @@
+import { readFile, writeFile } from 'node:fs/promises';
+import { join } from 'node:path';
+
+class Database {
+
+    constructor ( profileId ) {
+
+        this.profileId = profileId;
+        this.path = join( cwd, `data/${this.profileId}.json` );
+        this.data = null;
+
+    }
+
+    async loadGame () {
+
+        try {
+
+            const fileContent = await readFile( this.path, 'utf8' );
+            this.data = JSON.parse( fileContent );
+            return this.data;
+
+        } catch ( err ) {
+
+            if ( err.code === 'ENOENT' ) throw new Error( 'Game data file not found. Please initialize a new game first.' );
+            throw err;
+
+        }
+
+    }
+
+    async saveGame ( data ) {
+
+        await writeFile( this.path, JSON.stringify( data, null, 2 ), 'utf8' );
+        this.data = data;
+
+    }
+
+}
+
+const DB = new Database();
+await DB.loadGame();
+
+export { Database, DB };
