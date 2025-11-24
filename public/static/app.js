cf150d062a22580f93d71b65fb01f80408aa87e5
komed3 <pk97@outlook.de>
2025-11-24 12:14:22 +0100

    min fix

--- public/static/app.js
+++ public/static/app.js
@@ -1090,7 +1090,7 @@ class TruckFinancialTracker {
         const rows = this.data.assets.loans.map( l => ( [
             { class: 'currency', value: this.formatCurrency( l.amount ) },
             { value: this.formatDay( l.day ) },
-            { value: ( this.data.currentDay - l.day - 1 ) + ' / ' + l.term },
+            { value: Math.max( 0, this.data.currentDay - l.day - 1 ) + ' / ' + l.term },
             { value: `${ l.interestRate.toFixed( 1 ) }%` },
             { class: 'currency', value: this.formatCurrency( l.dailyInstallment ) },
             { class: 'currency', value: this.formatCurrency( l.remaining ) },
